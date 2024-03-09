d3d2930fcf1643ba98b4bb625ffd447a92032962
Rafael Scholz Griebler <rafaelscholz03@gmail.com>
2024-03-09 19:19:13 -0300

    Decode integers to better fitting data types

--- Examples/Pkl.ConsoleAppExample/Config/AppConfig.pkl.cs
+++ Examples/Pkl.ConsoleAppExample/Config/AppConfig.pkl.cs
@@ -4,7 +4,7 @@ public class AppConfig
 {
     public string Host { get; set; } = default!;
 
-    public int Port { get; set; } = default!;
+    public ushort Port { get; set; } = default!;
 
     public NestedConfig Nested { get; set; } = default!;
 }
