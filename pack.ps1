05549dda4215e0177583824d00dd1a7a6a2d5412
Jeremy Vaartjes <jeremyv@hey.com>
2024-03-19 00:11:37 +1100

    Add nuget config for generator project

--- pack.ps1
+++ pack.ps1
@@ -6,3 +6,4 @@ Write-Output "🥒 Packing nuget..."
 
 & dotnet pack Pkl --configuration Release --no-build --no-restore -o artifacts
 & dotnet pack Microsoft.Extensions.Configuration.Pkl --configuration Release --no-build --no-restore -o artifacts
+& dotnet pack PklCSharp.Generator --configuration Release --no-build --no-restore -o artifacts
